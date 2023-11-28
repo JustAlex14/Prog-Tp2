af43be2ae8af131f626c83298cce514d2cdb9e6b
Alex <98466389+JustAlex14@users.noreply.github.com>
2023-11-28 15:02:16 +0100

    q2.2-1

--- src/app/(login)/connexion/page.tsx
+++ src/app/(login)/connexion/page.tsx
@@ -5,10 +5,11 @@ import { useForm, zodResolver } from '@mantine/form';
 import { NumberInput, TextInput, Button, Box, Group, PasswordInput } from '@mantine/core';
 import Link from 'next/link';
 import {NoticeMessage, SectionContainer} from "tp-kit/components";
+import { useState } from 'react';
 
 const schema = z.object({
-  email: z.string().email({ message: 'Invalid email' }),
-  password: z.string().min(8, { message: 'Name should have at least 8 letters' }),
+  email: z.string().email({ message: 'Email Invalide' }),
+  password: z.string().min(8, { message: 'Le mot de passe doit faire minimum 8 lettres' }),
 });
 
 export default function InscriptionPage() {
@@ -20,40 +21,47 @@ export default function InscriptionPage() {
     },
   });
 
+  const [noticeMessage, setNoticeMessage] = useState([]);
+
+  function onFormSubmit(values : z.infer<typeof schema>) {
+    console.log(values)
+    if (values.name=="error") {
+      setNoticeMessage([{type: "error", message: "La connexion n'a pas pu s'effectuer, mail ou mdp invalid"}]);
+    }
+    else {
+      setNoticeMessage([{type: "success", message: "Vous êtes connecté!"}]);
+    }
+  }
+
   return (
-    
-    <Box maw={340} mx="auto" className="shadow-md my-5 bg-white rounded">
-      <form onSubmit={form.onSubmit((values: z.infer<typeof schema>) => console.log(values))} className="p-5">
-      <h1 className="mb-3">Connexion</h1>
+    <SectionContainer wrapperClassName="max-w-5xl">
+      <Box maw={340} mx="auto" className="shadow-md my-5 bg-white rounded">
+        <form onSubmit={form.onSubmit((values: z.infer<typeof schema>) => onFormSubmit(values))} className="p-5">
+        <h1 className="mb-3">Connexion</h1>
 
-      <NoticeMessage
-        message="Vous êtes connecté!"
-        onDismiss={function noRefCheck(){}}
-        type="success"
-      />
-      <NoticeMessage
-        message="La connexion n'a pas pu s'effectuer, mail ou mdp invalid"
-        onDismiss={function noRefCheck(){}}
-        type="error"
-      />
+          {noticeMessage.map((notice, i) => (
+            <NoticeMessage key={i}{...notice}/>
+          ))}
 
-        <TextInput
-          withAsterisk
-          label="Email"
-          placeholder="test@siuu.f"
-          {...form.getInputProps('email')}
-        />
-        <PasswordInput
-          withAsterisk
-          label="Password"
-          placeholder="Password"
-          {...form.getInputProps('password')}
-        />
+          <TextInput
+            withAsterisk
+            label="Email"
+            placeholder="test@siuu.f"
+            {...form.getInputProps('email')}
+          />
+          <PasswordInput
+            withAsterisk
+            label="Mot de passe"
+            placeholder="Mot de passe"
+            {...form.getInputProps('password')}
+          />
 
+          
+          <Button type="submit" className="bg-green-600 my-5 items-center hover:bg-green-600 h-12" fullWidth="true" >Se connecter</Button>
+          <Link href={'../inscription'}><p className="text-sm text-center text-green"> Vous n'avez pas de compte? Créez en un! </p></Link>
+        </form>
         
-        <Button type="submit" className="bg-green-600 my-5 items-center hover:bg-green-600 h-12" fullWidth="true" >Se connecter</Button>
-      </form>
-      <Link href={'../inscription'} className="text-sm text-center text-green"><p className="text-sm text-center text-green"> Vous n'avez pas de compte? Créez en un! </p></Link>
-    </Box>
+      </Box>
+    </SectionContainer>
   );
 }
--- src/app/(login)/inscription/page.tsx
+++ src/app/(login)/inscription/page.tsx
@@ -1,15 +1,16 @@
 "use client"
 
-import { z } from 'zod';
+import { Schema, z } from 'zod';
 import { useForm, zodResolver } from '@mantine/form';
 import { NumberInput, TextInput, Button, Box, Group, PasswordInput } from '@mantine/core';
 import Link from 'next/link';
-import { NoticeMessage } from 'tp-kit/components';
+import { NoticeMessage, SectionContainer } from 'tp-kit/components';
+import { useState } from 'react';
 
 const schema = z.object({
-  name: z.string().min(2, { message: 'Name should have at least 2 letters' }),
-  email: z.string().email({ message: 'Invalid email' }),
-  password: z.string().min(8, { message: 'Name should have at least 8 letters' }),
+  name: z.string().min(2, { message: 'Le nom doit faire minimum 2 lettres' }),
+  email: z.string().email({ message: 'Email Invalide' }),
+  password: z.string().min(8, { message: 'Le mot de passe doit faire minimum 8 lettres' }),
 });
 
 export default function InscriptionPage() {
@@ -22,48 +23,57 @@ export default function InscriptionPage() {
     },
   });
 
+
+  const [noticeMessage, setNoticeMessage] = useState([]);
+
+  function onFormSubmit(values : z.infer<typeof schema>) {
+    console.log(values)
+    if (values.name=="error") {
+      setNoticeMessage([{type: "error", message: "Une erreur s'est produite !"}]);
+    }
+    else {
+      setNoticeMessage([{type: "success", message: "Votre inscription a bien été prise en compte, vous pouvez vous connecter."}]);
+    }
+  }
+
   return (
-    <Box maw={340} mx="auto" className="shadow-md my-5 bg-white rounded">
-      <form onSubmit={form.onSubmit((values: z.infer<typeof schema>) => console.log(values))} className="p-5">
-      <h1 className="mb-3">INSCRIPTION</h1>
+    <SectionContainer wrapperClassName="max-w-5xl">
+      <Box maw={340} mx="auto" className="shadow-md my-5 bg-white rounded">
 
-      <NoticeMessage
-        message="Votre inscription a bien été prise en compte, vous pouvez vous connecter."
-        onDismiss={function noRefCheck(){}}
-        type="success"
-      />
-      <NoticeMessage
-        message="Une erreur s'est produite !"
-        onDismiss={function noRefCheck(){}}
-        type="error"
-/>
-
-      <TextInput
-          withAsterisk
-          label="Name"
-          placeholder="John Doe"
-          mt="sm"
-          {...form.getInputProps('name')}
-        />
+        <form onSubmit={form.onSubmit((values: z.infer<typeof schema>) => onFormSubmit(values))} className="p-5">
+        <h1 className="mb-3">INSCRIPTION</h1>
         
+        {noticeMessage.map((notice, i) => (
+          <NoticeMessage key={i}{...notice}/>
+        ))}
+
         <TextInput
-          withAsterisk
-          label="Email"
-          placeholder="test@siuu.f"
-          {...form.getInputProps('email')}
-        />
-        <PasswordInput
-          withAsterisk
-          label="Password"
-          placeholder="Password"
-          {...form.getInputProps('password')}
-        />
+            withAsterisk
+            label="Nom"
+            placeholder="John Doe"
+            mt="sm"
+            {...form.getInputProps('name')}
+          />
+          
+          <TextInput
+            withAsterisk
+            label="Email"
+            placeholder="test@siuu.f"
+            {...form.getInputProps('email')}
+          />
+          <PasswordInput
+            withAsterisk
+            label="Mot de passe"
+            placeholder="Mot de passe"
+            {...form.getInputProps('password')}
+          />
 
+          
+          <Button type="submit" className="bg-green-600 my-5 items-center hover:bg-green-600 h-12" fullWidth="true" >S'inscrire</Button>
+          <Link href={'../connexion'}> <p className="text-sm text-center text-green">Vous avez un compte? Connectez vous!</p></Link>
+        </form>
         
-        <Button type="submit" className="bg-green-600 my-5 items-center hover:bg-green-600 h-12" fullWidth="true" >S'inscrire</Button>
-
-      </form>
-      <Link href={'../connexion'} className="text-sm text-center text-green"> <p className="text-sm text-center text-green">Vous avez un compte? Connectez vous!</p></Link>
-    </Box>
+      </Box>
+    </SectionContainer>
   );
 }
